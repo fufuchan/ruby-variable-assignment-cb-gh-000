9c38a7994958d7f2859972bdc1690857abb710e4
Freya Berkhout <freya.berkhout@protonmail.com>
2018-10-26 17:29:01 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,2 @@
-greeting = "Hello World!" # Defgrreine a local variable called 'greeting',
+greeting = "Hello World" # Defgrreine a local variable called 'greeting',
  # give it the value of the string "Hello World"
